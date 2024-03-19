73ce32caf489eba40dfc4f10035801698ee287eb
srivaishnavi-02 <smallip1@uncc.edu>
2024-03-19 10:28:44 -0400

    added db

---
+++ models/story.js
@@ -0,0 +1,41 @@
+const { DateTime } = require("luxon");
+const {v4: uuidv4} = require('uuid');
+const {ObjectId} = require('mongodb');
+
+let stories;
+
+exports.getCollection = db=>{
+    stories = db.collection('stories');
+}
+
+exports.find = () => stories.find().toArray();
+
+exports.findById = id => stories.findOne({_id: ObjectId(id)});
+
+exports.save = function (story) {
+    story.id = uuidv4();
+    story.createdAt = DateTime.now().toLocaleString(DateTime.DATETIME_SHORT);
+    stories.push(story);
+};
+
+exports.updateById = function(id, newStory) {
+    let story = stories.find(story=>story.id === id);
+    if(story) {
+        story.title = newStory.title;
+        story.content = newStory.content;
+        return true;
+    } else {
+        return false;
+    }
+ 
+}
+
+exports.deleteById = function(id) {
+    let index = stories.findIndex(story =>story.id === id);
+    if(index !== -1) {
+        stories.splice(index, 1);
+        return true;
+    } else {
+        return false;
+    }
+}
